9b827a7370aaab4922ed3b670e2f735f7f561daa
ningxiaoxiao <732869825@qq.com>
2017-10-12 18:01:48 +0800

    更新调试器

--- Crawler/Scheduler/ISchduler.cs
+++ Crawler/Scheduler/ISchduler.cs
@@ -5,6 +5,22 @@ namespace Crawler.Core.Scheduler
     public interface ISchduler
     {
         /// <summary>
+        /// 向调试中心增加一个地址
+        /// </summary>
+        /// <param name="url"></param>
+        void AddUrl(string url);
+        /// <summary>
+        /// 向调试中心增加很多个地址
+        /// </summary>
+        /// <param name="urls"></param>
+        void AddUrls(IDisposable urls);
+
+        void GetNextUrl()
+        {
+            
+        }
+
+        /// <summary>
         /// 处理结果
         /// </summary>
         /// <param name="resultItems">抽取出来的结果</param>
